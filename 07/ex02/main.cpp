7e46defaf6d87a17f66bab4020f0f33b622420a1
Guillaume Yvergniaux <gyvergni@z3r2p1.42lyon.fr>
2024-12-13 13:32:29 +0100

    07 clean

--- 07/ex02/main.cpp
+++ 07/ex02/main.cpp
@@ -1,51 +1,53 @@
-#include "Array.hpp"
 #include <iostream>
-#include <cstdlib>
+#include "Array.hpp"
 
-int	main(void)
+#define MAX_VAL 750
+int main(int, char**)
 {
-	Array<int> numbers(50);
-	int *mirror = new int[50];
-	srand(time(NULL));
-	for (int i = 0; i < 50; i++)
-	{
-		const int value = rand();
-		numbers[i] = value;
-		mirror[i] = value;
-	}
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
 
-	{
-		Array<int> tmp;
-		try
-		{
-			for (int i = 0; i < 50; i++)
-				std::cout << tmp[i] << std::endl;
-		}
-		catch(const std::exception &e)
-		{
-			std::cerr << e.what() << '\n';
-		}
-		tmp = numbers;
-		Array<int> test(tmp);
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-		try
-		{
-			for (int i = 0; i < 51; i++)
-			{
-				std::cout << tmp[i] << std::endl;
-				if (tmp[i] != test[i])
-				{
-					std::cerr << "assignement constructor didn't copy the same value";
-					return 1;
-				}
-			}
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
-
-		std::cout << "tmp size is: " << tmp.size() << std::endl;	
-	}
-	delete[] mirror;
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+    return 0;
 }
