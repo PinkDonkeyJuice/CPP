1b7053b78afe8a12077933c3a9bbd81deb924d65
PinkDonkeyJuice <yvergniauxg@gmail.com>
2025-01-08 19:43:25 +0100

    ex01 done

--- 09/ex01/RPN.cpp
+++ 09/ex01/RPN.cpp
@@ -26,24 +26,30 @@ RPN	&RPN::operator=(const RPN &ref)
 
 void	RPN::do_op(std::string buffer)
 {
-	int	val1;
-	int val2;
-	int result;
+	long long val1;
+	long long val2;
+	long long result;
 
 	if (buffer.length() != 1)
 		throw(std::exception());
-	val1 = this->_stack.top();
+	val1 = static_cast<long long>(this->_stack.top());
 	this->_stack.pop();
-	val2 = this->_stack.top();
+	val2 = static_cast<long long>(this->_stack.top());
 	if (buffer[0] == '+')
 		result = val1 + val2;
 	if (buffer[0] == '-')
 		result = val2 - val1;
 	if (buffer[0] == '*')
 		result = val2 * val1;
-	if (buffer[0] == '/' )
+	if (buffer[0] == '/')
+	{
+		if (val1 == 0)
+			throw(std::exception());
 		result = val2 / val1;
-	this->_stack.push(result);
+	}
+	if (result > 2147483647 || result < -2147483648)
+		throw(std::exception());
+	this->_stack.push(static_cast<int>(result));
 }		
 
 void	RPN::calculate()
@@ -66,10 +72,11 @@ void	RPN::calculate()
 			throw(std::exception());
 		}
 		this->_stack.push(value);
-		if (std::getline(ss, buffer, ' '))
+		if (!std::getline(ss, buffer, ' '))
 		{
-			do_op(buffer);
+			throw(std::exception());
 		}
+		do_op(buffer);
 	}
 	std::cout << this->_stack.top() << std::endl;
 }
